e1bd697e91c10106a22aeb797a260442bc1fdc4e
kumavis <kumavis@users.noreply.github.com>
2023-12-14 03:07:01 +0000

    Deploying to gh-pages from @ kumavis/reddit-images@6b5ce4f111beae1f30451719a30dd1c2e34b3f73 🚀

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import React from 'react'
-import { Root } from './containers/Root.js'
-
-const h = React.createElement
-
-const container = document.getElementById('app-content')
-const root = createRoot(container)
-root.render(h(Root))
